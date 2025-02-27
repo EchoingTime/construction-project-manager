e83307dd50aaeef34c5aa5836061ae10d8e21571
Dante A. Anzalone <bookofechoes@gmail.com>
2025-02-27 14:11:11 -0500

    Directions Updated

--- main.py
+++ main.py
@@ -11,8 +11,10 @@
         - Optional: Set Flask environment variable:
             - export FLASK_APP=main.py (Mac/Linux)  # Set the app to main.py
             - set FLASK_APP=main.py (Windows)       # Set the app to main.py
+            - flask run                             # Run the Flask app
         - Or you can run it directly:
             - python main.py                        # Alternatively, run Flask directly with python
+    - Testing Account: test@test.com | Test1234
     - Control + C to stop the server
 @References
     - https://www.youtube.com/watch?v=dam0GPOAvVI&ab_channel=TechWithTim
