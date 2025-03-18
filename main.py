f4fb0f1a20f303d25e082841146660c3326f51e5
Dante A. Anzalone <bookofechoes@gmail.com>
2025-03-18 12:33:40 -0400

    Python Version Update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 @Running:
     - Open Visual Studio Code's terminal
     - Run the following commands:
-        - python -m venv venv (if venv doesn't already exist)
+        - python3 -m venv venv (if venv doesn't already exist)
         - source venv/bin/activate (Mac/Linux)
         - venv\Scripts\activate (Windows)
         - pip install -r requirements.txt
-        - python main.py
+        - python3 main.py
     - Testing Account: test@test.com | test123
     - Test Message Account: test2@test.com | test123
     - Control + C to stop the server
