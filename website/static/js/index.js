e0fd44131784072da4093afdbd1ec2f67dec5c1d
Dante A. Anzalone <bookofechoes@gmail.com>
2025-04-19 17:09:21 -0400

    Margin Corrections and Made Edit Toggle Universal

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -264,12 +264,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-/*--------------------- Toggle Projection Edit ---------------------*/
+/*--------------------- Toggle Edit ---------------------*/
 
-function toggleEdit() {
-  const deleteBtn = document.querySelectorAll("#delete-button");
-  const editIcon = document.querySelector("#edit-project");
+function toggleEdit(editButtonId, sectionId) {
+  const deleteBtn = document.querySelectorAll(`#${sectionId} #delete-button`);
+  const editIcon = document.querySelector(`#${editButtonId}`);
 
+  // Toggling between svg states
   if (editIcon.innerHTML.includes("M200-200h57l391")) {
     editIcon.innerHTML =
       '<path d="m622-453-56-56 82-82-57-57-82 82-56-56 195-195q12-12 26.5-17.5T705-840q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L622-453ZM200-200h57l195-195-28-29-29-28-195 195v57ZM792-56 509-338 290-120H120v-169l219-219L56-792l57-57 736 736-57 57Zm-32-648-56-56 56 56Zm-169 56 57 57-57-57ZM424-424l-29-28 57 57-28-29Z" />';
@@ -278,9 +279,9 @@ function toggleEdit() {
       '<path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />';
   }
 
-  // Only for Project Overview
-  const projectOverview = document.getElementById("project-section");
-  if (projectOverview) {
+  // Toggle visibility of deleete buttons in their target section
+  const section = document.getElementById(sectionId);
+  if (section) {
     deleteBtn.forEach((btn) => {
       btn.classList.toggle("hidden-button");
     });
